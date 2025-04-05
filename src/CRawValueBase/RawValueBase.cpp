1d258e0d57c7315f5d022994b5df8230aafeaea8
alireza <alireza1993jafari@gmail.com>
2025-04-05 14:49:28 +0430

    [REF] style correction

--- src/CRawValueBase/RawValueBase.cpp
+++ src/CRawValueBase/RawValueBase.cpp
@@ -3,10 +3,7 @@
 
 
 void CValue_int::accept         (CValueVisitor& visitor) { visitor.visit(*this); }
-
-
 void CValue_double::accept      (CValueVisitor& visitor) { visitor.visit(*this); }
 void CValue_string::accept      (CValueVisitor& visitor) { visitor.visit(*this); }
 void CValue_bool::accept        (CValueVisitor& visitor) { visitor.visit(*this); }
-
 void CValue_map::accept         (CValueVisitor& visitor) { visitor.visit(*this); }
